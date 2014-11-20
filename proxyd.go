fbdb1023bdc41d56145752d2d971f07d3015d0f6
Steven Yue <jincheker@gmail.com>
2014-11-20 00:08:11 -0500

    Added request log.

--- proxyd.go
+++ proxyd.go
@@ -1,29 +1,64 @@
 package main
 
 import (
-	"github.com/elazarl/goproxy"
-	"log"
 	"flag"
+	"github.com/elazarl/goproxy"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
-	"io/ioutil"
 	"strconv"
 )
 
+func GetIP(r *http.Request) string {
+	if ipProxy := r.Header.Get("X-FORWARDED-FOR"); len(ipProxy) > 0 {
+		return ipProxy
+	}
+
+	return r.RemoteAddr
+}
+
 func main() {
-	verbose := flag.Bool("v", false, "should every proxy request be logged to stdout")
-	addr := flag.String("addr", ":8080", "proxy listen address")
-	pidFile := flag.String("pid", "", "the pid file")
-        flag.Parse()
-        
+	verbose := flag.Bool("v", false, "print verbose output")
+	addr := flag.String("a", ":8080", "proxy listen address")
+	pidfile := flag.String("p", "", "path to pidfile")
+	logfile := flag.String("l", "", "path to writable logfile")
+
+	flag.Parse()
+
+	// Write log file.
+	var logger *log.Logger
+	if *logfile != "" {
+		log_f, err := os.OpenFile(*logfile, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0600)
+		if err == nil {
+			defer log_f.Close()
+			logger = log.New(log_f, "proxyd: ", log.LstdFlags)
+		} else {
+			log.Printf("Failed to open log file %s : %v", log_f, err)
+			os.Exit(1)
+		}
+	} else {
+		logger = log.New(os.Stdout, "proxyd: ", log.LstdFlags)
+	}
+
 	// Write pid file.
-	if *pidFile != "" {
+	if *pidfile != "" {
 		pid := strconv.Itoa(os.Getpid())
-		if err := ioutil.WriteFile(*pidFile, []byte(pid), 0644); err != nil {
-			panic(err)
+		if err := ioutil.WriteFile(*pidfile, []byte(pid), 0644); err != nil {
+			log.Printf("Failed to write to pid file : %v", err)
+			os.Exit(1)
 		}
 	}
+
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.Verbose = *verbose
+
+	// Log requests
+	proxy.OnRequest().DoFunc(func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+		logger.Printf("Request: %s | IP: %s", req.URL, GetIP(req))
+		// If the HTTP response is not nil, the proxy will never send the request to the remote client
+		return req, nil
+	})
+
 	log.Fatal(http.ListenAndServe(*addr, proxy))
 }
